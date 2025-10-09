8e76dd98cc73394c0d595e87724b6e9c1f2def9f
Aiden A <76793340+Mindstormman06@users.noreply.github.com>
2025-10-09 09:15:47 -0700

    Moved mascot

--- mascot.php
+++ mascot.php
@@ -1,3 +1,3 @@
 <div id="mascot" class="fixed left-6 z-50" style="bottom: -1rem;">
-  <img src="/uploads/mascot.png" alt="Mascot" class="w-12 drop-shadow-xl" style="image-rendering: auto;">
+  <img src="/assets/mascot.png" alt="Mascot" class="w-12 drop-shadow-xl" style="image-rendering: auto;">
 </div>
